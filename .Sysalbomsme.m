91978cce5d6c015b44a18b6d58621f0a023e3639
danialzivehdar1992@gmail.com <243804543+danialzivehdadr@users.noreply.github.com>
2026-08-03 01:48:28 +0330

    Update .Sysalbomsme.m
    Signed-off-by: danialzivehdar1992@gmail.com   <243804543+danialzivehdadr@users.noreply.github.com>

--- .Sysalbomsme.m
+++ .Sysalbomsme.m
@@ -5,11 +5,11 @@ Join bio name aame "a, s"!,
 Join  heom auo uo "ok repeat" 
 uotnd kare on "ok baby" 
 den ffffffffffffffffff do ro. 'nd'
-Join folder url uo "ok 😘"
+Join folder url uo "ok.fotenk"
 sys foder Join annste virus! "dede"
 ran system in  
 a 1 on (e) var daewqp 
-((addres lock _^) open Al`
+((addres lock _^) name`
 all adder 
 inaddress ll  
 URL: electron
