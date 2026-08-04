4d77c7df7aaab4d73bd53068de60fd318a252ccf
danialzivehdar1992@gmail.com <243804543+danialzivehdadr@users.noreply.github.com>
2026-08-04 16:33:55 +0330

    Update sysheom_&com.py
    Signed-off-by: danialzivehdar1992@gmail.com   <243804543+danialzivehdadr@users.noreply.github.com>

--- assets/css/extended/sysheom_&com.py
+++ assets/css/extended/sysheom_&com.py
@@ -1,6 +1,6 @@
-eion eint i 
-eione settings paly [
-[] $$$
+eion eint
+eione settings_paly[
+@ []
 phtoh sys,system = viardear mp,
 copy dearspe&&(0000)
 Join bio nameaame!"a, s",
@@ -8,16 +8,16 @@ Join heom, work,auouo"okrepeat"
 uotnd kare on"okbaby" 
 [
 den(up addrs)doro"n/d"
-      Join folder url uo"ok_fotenk"
-    sys foder Join annste virus"dede"
+Join folder url uo"ok_fotenk"
+sys foder Join annste virus"dede"
 ran system in 1on (e)vardaewqp 
 ````
 ((addres lock _^) dos System `uozrname`
-  all adder inaddress ==ll 
-   URL:folderviewnaem on"dede"
-   Jion videopeixpfiles"dedero"
-  phtoh fodere(docs, pdf, file, files,)
- deputy gin(dede)
+all adder inaddress == ll;
+url:folderviewnaem on"dede"
+Jion (Ul) videopeixpfiles"dedero"
+phtoh fodere(docs, pdf, file, files,)
+deputy gin(dede)
 URL: (ro_recycling_res"no")
 ‭ln(^(-1)atan(acos(lg(3e))))
 ********denxiup********
